96d548e7b7da68f11b25c6912468cbb89dcd4798
Natalia Eremina <entalia663@gmail.com>
2023-08-01 22:03:00 +0200

    Task 4 was finished

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 32, b = 84, c = 14;
+
+if (a > b){
+    if (a > c){
+        Console.WriteLine(a);
+    }
+    else Console.WriteLine(c);
+}
+
+else {
+    if (b > c){
+        Console.WriteLine(b);
+    }
+    else Console.WriteLine(c);
+}
