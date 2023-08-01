8e43ae422fe7d514e0293729b413dad5c8945da0
Natalia Eremina <entalia663@gmail.com>
2023-08-01 23:01:51 +0200

    Task 8 was finished

---
+++ Task_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter your number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+
+while (a >= num){
+    if (num % 2 == 0){
+        Console.WriteLine(num);
+    }
+    num++;
+}
