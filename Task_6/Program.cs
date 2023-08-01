cfbba02c15c6a5391a08e5c967a2f667429a62d5
Natalia Eremina <entalia663@gmail.com>
2023-08-01 22:23:22 +0200

    Task 6 was finished

---
+++ Task_6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter your number: ");
+int a = Convert.ToInt32(Console.Read());
+
+if (a % 2 == 0){
+    Console.WriteLine("Number is even");
+}
+else Console.WriteLine("Number is odd");
